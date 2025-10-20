f1d1aaf92837cbfd0d697e454972b09864a92b49
floto <settheskibidi@gmail.com>
2025-10-20 17:13:53 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,18 @@
+(function() {
+  // Wait until the game object is ready
+  function unlockAchievements() {
+    if (typeof Game !== "undefined" && Game.Achievements) {
+      for (let ach of Game.Achievements) {
+        if (ach.won !== 1) {
+          Game.Win(ach.name);
+        }
+      }
+      console.log("All achievements unlocked!");
+    } else {
+      // Retry if Game is not ready
+      setTimeout(unlockAchievements, 1000);
+    }
+  }
+  unlockAchievements();
+})();
 
